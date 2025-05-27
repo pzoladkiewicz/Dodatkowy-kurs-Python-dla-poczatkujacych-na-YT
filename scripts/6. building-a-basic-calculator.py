3ec0fc109fe3b65fb5ff7b753bb691b89674c754
pzoladkiewicz <pawel.zoladkiewicz@windowslive.com>
2025-05-27 12:28:19 +0200

    Create 6. building-a-basic-calculator.py

---
+++ scripts/6. building-a-basic-calculator.py
@@ -0,0 +1,9 @@
+num1 = input("Enter a number: ")                # 5
+num2 = input("Enter another number: ")          # 8.3
+result = num1 + num2                            # 58.3 // str + str
+print(result)                               
+
+num1 = input("Enter a number: ")                # 5
+num2 = input("Enter another number: ")          # 8.3
+result = int(num1) + float(num2)                # 13.3 
+print(result)
