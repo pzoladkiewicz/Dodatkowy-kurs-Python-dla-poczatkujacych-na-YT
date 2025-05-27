7ea2f53d2acf1b608602fd63fc9a95a3b8854147
pzoladkiewicz <pawel.zoladkiewicz@windowslive.com>
2025-05-27 12:28:59 +0200

    Create 7. mad-libs-game.py

---
+++ scripts/7. mad-libs-game.py
@@ -0,0 +1,7 @@
+color = input("Enter a color: ")
+plurar_noun = input("Enter a Plurar Noun: ")
+celebrity = input("Enter a celebrity: ")
+
+print("Roses are " + color)
+print(plurar_noun + " are blue")
+print("I love " + celebrity)
