cab8108dcb0c825d74fefb4028ac699a2bdfa08a
pzoladkiewicz <pawel.zoladkiewicz@windowslive.com>
2025-05-27 12:29:28 +0200

    Create 8. lists.py

---
+++ scripts/8. lists.py
@@ -0,0 +1,11 @@
+list_example = ["Kevin", 2 , False]     # anything
+
+friends = ["Kevin", "Karen", "Jim", "Oscar", "Toby"]
+print(friends)
+print(friends[0])                       # Kevin
+print(friends[-1])                      # Jim
+print(friends[3:])                      # ['Oscar', 'Toby']
+print(friends[1:3])                     # ['Karen', 'Jim']
+
+friends[1] = 'Mike'
+print(friends[1])                       # Mike
