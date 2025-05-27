374defc8f81d74216f69f93bdfec37a55d9b8ed7
pzoladkiewicz <pawel.zoladkiewicz@windowslive.com>
2025-05-27 12:16:40 +0200

    Create variables-and-data-types.py

---
+++ scripts/variables-and-data-types.py
@@ -0,0 +1,28 @@
+print("There once was a man named George, ")
+print("he was 70 years old. ")
+print("He really liked the name George, ")
+print("but didn't like being 70.")
+
+character_name = "John"
+character_age = "35"
+print("There once was a man named " + character_name + ", ")
+print("he was " + character_age + " years old. ")
+print("He really liked the name " + character_name + ", ")
+print("but didn't like being " + character_age + ".")
+
+character_name = "John"
+character_age = "35"
+print("There once was a man named " + character_name + ", ")
+print("he was " + character_age + " years old. ")
+
+character_name = "Mike"
+print("He really liked the name " + character_name + ", ")
+print("but didn't like being " + character_age + ".")
+
+character_name = "John"         # string
+character_age = 35              # number
+is_male = True                  # boolean
+print("There once was a man named " + character_name + ", ")
+print("he was " + str(character_age) + " years old. ")
+print("He really liked the name " + character_name + ", ")
+print("but didn't like being " + str(character_age) + ".")
