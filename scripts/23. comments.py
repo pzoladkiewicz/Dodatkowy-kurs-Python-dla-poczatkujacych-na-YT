4c71e93096860e3e1624ddeaf4781fad5622731c
pzoladkiewicz <pawel.zoladkiewicz@windowslive.com>
2025-05-30 13:06:32 +0200

    Create 23. comments.py

---
+++ scripts/23. comments.py
@@ -0,0 +1,12 @@
+print("Comments are cool")
+  
+
+# Recommended -> # at start
+# notes or comments
+# temporarily commenting out a line of code
+
+
+'''
+The unofficial way
+to comment block
+'''  
