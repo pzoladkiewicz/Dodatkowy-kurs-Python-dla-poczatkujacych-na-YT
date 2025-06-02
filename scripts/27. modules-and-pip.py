c0cbcf8830e1b0386a986721ecb5d29fe7a16bd6
pzoladkiewicz <pawel.zoladkiewicz@windowslive.com>
2025-06-02 08:14:25 +0200

    Create 27. modules-and-pip.py

---
+++ scripts/27. modules-and-pip.py
@@ -0,0 +1,21 @@
+
+# create file useful_tools.py
+# save to C:\GiraffeAcademy
+
+import useful_tools
+
+print(useful_tools.roll_dice(10))
+
+
+##############################################################
+
+# https://docs.python.org/3/py-modindex.html
+# pip install python-docx
+# > site-packages > docx
+
+import docx
+
+doc = docx.Document()
+
+
+# pip uninstall python-docx
