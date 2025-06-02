0f9e78d339d951ffef9ad08694ae6ba3e4d19bf0
pzoladkiewicz <pawel.zoladkiewicz@windowslive.com>
2025-06-02 10:44:25 +0200

    Create 28. classes-and-objects.py

---
+++ scripts/28. classes-and-objects.py
@@ -0,0 +1,7 @@
+from Student import Student
+
+student1 = Student("Jim", "Business", 3.1, False)
+student2 = Student("PAM", "Art", 2.5, True)
+
+print(student1.name)                            #   Jim
+print(student2.gpa)                             #   2.5
